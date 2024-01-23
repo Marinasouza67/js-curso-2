1107186412e84b2b2394c7b00d47acf5eb6d1008
Marinasouza67 <156912762+Marinasouza67@users.noreply.github.com>
2024-01-23 10:44:26 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector ("h1");
+titulo.innerHTML= " Hora do desafio";
+
+function  verificarChute(){
+    console.log = ("o botão foi clicado");
+}
+function button  v(){
+    alert = ("Eu amo JS ");
+  function 
